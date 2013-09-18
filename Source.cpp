a425b66b98a1710ce73cfdedbbb003f2afdeb162
Parthas-Menethil <xywhpanda@gmail.com>
2013-09-18 13:14:53 -0400

    Introduced typedef

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,13 @@
+typedef int color;
+
+
 void minusOne(void* a){
 	*(int*)a -= 1;
 }
 
 int main(){
 	int a = 10;
+	color c = 0;
 	long long b = 10;
 	minusOne(&a);
 	minusOne((int*)&b); // Fixed Type Error
